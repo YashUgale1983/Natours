7f1e9bfddfd321773e137f3317dadd30d2da8a48
Yash Ugale <yashugale1983@gmail.com>
2023-07-05 00:25:40 +0530

    Deployed redis server on render and modified URL

--- redis.js
+++ redis.js
@@ -2,7 +2,9 @@
 
 const redis = require("redis"); // using redis@3.1.2
 
-const redisURL = "redis://127.0.0.1:" + process.env.REDIS_PORT; // defining redis port
+// const redisURL = "redis://127.0.0.1:" + process.env.REDIS_PORT; // defining redis port
+const redisURL = process.env.REDIS_URL; // defining redis port
+
 const client = redis.createClient(redisURL); // creating a client instance
 if (client) console.log("Redis DB set up on port : " + process.env.REDIS_PORT); // logging if connection successful
 
